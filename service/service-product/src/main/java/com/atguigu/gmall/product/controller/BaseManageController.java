6152ddf297b3e8660d0f1a1451a5343290076149
mmycode <845443900@qq.com>
2021-03-31 14:13:28 +0800

    添加商品spu

--- service/service-product/src/main/java/com/atguigu/gmall/product/controller/BaseManageController.java
+++ service/service-product/src/main/java/com/atguigu/gmall/product/controller/BaseManageController.java
@@ -1,14 +1,21 @@
 package com.atguigu.gmall.product.controller;
 
 import com.atguigu.gmall.common.result.Result;
-import com.atguigu.gmall.model.product.BaseAttrInfo;
-import com.atguigu.gmall.model.product.BaseCategory1;
-import com.atguigu.gmall.model.product.BaseCategory2;
-import com.atguigu.gmall.model.product.BaseCategory3;
+import com.atguigu.gmall.model.product.*;
 import com.atguigu.gmall.product.service.BaseManageService;
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.apache.commons.io.FilenameUtils;
+import org.csource.fastdfs.ClientGlobal;
+import org.csource.fastdfs.StorageClient1;
+import org.csource.fastdfs.TrackerClient;
+import org.csource.fastdfs.TrackerServer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.util.ClassUtils;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -44,5 +51,54 @@ public class BaseManageController {
                                                    @PathVariable Long category3Id) {
         List<BaseAttrInfo> list = baseManageService.attrInfoList(category1Id, category2Id, category3Id);
         return Result.ok(list);
+    }
+
+    @ApiOperation("获取spu分页列表")
+    @GetMapping("/{page}/{limit}")
+    public Result<IPage<SpuInfo>> getSpuInfo(@PathVariable long page, @PathVariable long limit, long category3Id) {
+        IPage<SpuInfo> spuInfoIPage = baseManageService.getSpuInfo(page,limit,category3Id);
+        return Result.ok(spuInfoIPage);
+    }
+
+    @ApiOperation("获取销售属性")
+    @GetMapping("/baseSaleAttrList")
+    public Result<List<BaseSaleAttr>> getBaseSaleAttr(){
+        List<BaseSaleAttr> list = baseManageService.getBaseSaleAttr();
+        return Result.ok(list);
+    }
+
+    @GetMapping("/baseTrademark/getTrademarkList")
+    public Result<List<BaseTrademark>> getTrademarkList(){
+        List<BaseTrademark> list = baseManageService.getTrademarkList();
+        return Result.ok(list);
+    }
+
+    @PostMapping("/saveSpuInfo")
+    public Result saveSpuInfo(@RequestBody SpuInfo spuInfo){
+        baseManageService.saveSpuInfo(spuInfo);
+        return Result.ok();
+    }
+
+    @Value("${image.url}")
+    private String imageUrl;
+
+    @PostMapping("/fileUpload")
+    public Result<String> fileUpload(MultipartFile file) throws Exception{
+
+        //加载配置文件tracker.conf
+        String allPath = ClassUtils.getDefaultClassLoader().getResource("tracker.conf").getPath();
+        //1.初始化配置文件
+        ClientGlobal.init(allPath);
+        //2.连接Tracker跟踪器，获取存储节点的地址
+        TrackerClient trackerClient = new TrackerClient();
+        //3.tracker跟踪器 服务器，返回的地址。
+        TrackerServer trackerServer = trackerClient.getConnection();
+        //4.连接存储节点
+        StorageClient1 storageClient1 = new StorageClient1(trackerServer, null);
+        //获取扩展文件名
+        String extension = FilenameUtils.getExtension(file.getOriginalFilename());
+        //5.保存文件并返回文件路径
+        String path = storageClient1.upload_file1(file.getBytes(), extension, null);
+        return Result.ok(imageUrl + path);
     }
 }
--- service/service-product/src/main/java/com/atguigu/gmall/product/service/impl/BaseManageServiceImpl.java
+++ service/service-product/src/main/java/com/atguigu/gmall/product/service/impl/BaseManageServiceImpl.java
@@ -1,15 +1,11 @@
 package com.atguigu.gmall.product.service.impl;
 
-import com.atguigu.gmall.model.product.BaseAttrInfo;
-import com.atguigu.gmall.model.product.BaseCategory1;
-import com.atguigu.gmall.model.product.BaseCategory2;
-import com.atguigu.gmall.model.product.BaseCategory3;
-import com.atguigu.gmall.product.mapper.BaseAttrInfoMapper;
-import com.atguigu.gmall.product.mapper.BaseCategory1Mapper;
-import com.atguigu.gmall.product.mapper.BaseCategory2Mapper;
-import com.atguigu.gmall.product.mapper.BaseCategory3Mapper;
+import com.atguigu.gmall.model.product.*;
+import com.atguigu.gmall.product.mapper.*;
 import com.atguigu.gmall.product.service.BaseManageService;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -30,6 +26,12 @@ public class BaseManageServiceImpl implements BaseManageService {
     @Autowired
     private BaseAttrInfoMapper baseAttrInfoMapper;
 
+    @Autowired
+    private SpuInfoMapper spuInfoMapper;
+
+    @Autowired
+    private BaseSaleAttrMapper baseSaleAttrMapper;
+
     @Override
     public List<BaseCategory1> getCategory1() {
         return baseCategory1Mapper.selectList(new QueryWrapper<>());
@@ -48,5 +50,66 @@ public class BaseManageServiceImpl implements BaseManageService {
     @Override
     public List<BaseAttrInfo> attrInfoList(Long category1Id, Long category2Id, Long category3Id) {
         return baseAttrInfoMapper.getAttrInfoList(category1Id, category2Id, category3Id);
+    }
+
+    @Override
+    public IPage<SpuInfo> getSpuInfo(long page, long limit, long category3Id) {
+        IPage<SpuInfo> iPage = new Page<>(page, limit);
+        QueryWrapper<SpuInfo> wrapper = new QueryWrapper<>();
+
+        wrapper.eq("category3_id", category3Id);
+        return spuInfoMapper.selectPage(iPage, wrapper);
+    }
+
+    @Override
+    public List<BaseSaleAttr> getBaseSaleAttr() {
+        return baseSaleAttrMapper.selectList(new QueryWrapper<BaseSaleAttr>().orderByAsc("id"));
+    }
+
+    @Autowired
+    private BaseTrademarkMapper baseTrademarkMapper;
+
+    @Autowired
+    private SpuImageMapper spuImageMapper;
+
+    @Autowired
+    private SpuSaleAttrMapper spuSaleAttrMapper;
+
+    @Autowired
+    private SpuSaleAttrValueMapper spuSaleAttrValueMapper;
+
+    @Override
+    public List<BaseTrademark> getTrademarkList() {
+        return baseTrademarkMapper.selectList(null);
+    }
+
+    @Override
+    public void saveSpuInfo(SpuInfo spuInfo) {
+        //1.保存品牌信息
+        int insert = spuInfoMapper.insert(spuInfo);
+        if (insert != 0) {
+            //2.保存品牌图片
+            Long spuId = spuInfo.getId();
+            List<SpuImage> spuImageList = spuInfo.getSpuImageList();
+            spuImageList.forEach(spuImage -> {
+                spuImage.setSpuId(spuId);
+                spuImageMapper.insert(spuImage);
+            });
+            //3.保存销售属性
+            List<SpuSaleAttr> spuSaleAttrList = spuInfo.getSpuSaleAttrList();
+            spuSaleAttrList.forEach(spuSaleAttr -> {
+                spuSaleAttr.setSpuId(spuId);
+                int insert1 = spuSaleAttrMapper.insert(spuSaleAttr);
+                if (insert1 != 0) {
+                    //4.保存销售属性值表
+                    List<SpuSaleAttrValue> spuSaleAttrValueList = spuSaleAttr.getSpuSaleAttrValueList();
+                    spuSaleAttrValueList.forEach(spuSaleAttrValue -> {
+                        spuSaleAttrValue.setSpuId(spuId);
+                        spuSaleAttrValue.setSaleAttrName(spuSaleAttr.getSaleAttrName());
+                        spuSaleAttrValueMapper.insert(spuSaleAttrValue);
+                    });
+                }
+            });
+        }
     }
 }
