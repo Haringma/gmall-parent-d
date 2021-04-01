83a8f9f15f01c3ab550fb1d6a632b614bad1238a
mmycode <845443900@qq.com>
2021-04-01 09:24:32 +0800

    降级处理

--- service-client/service-product-client/src/main/java/com/atguigu/gmall/fallback/ProductDegradeFeignClient.java
+++ service-client/service-product-client/src/main/java/com/atguigu/gmall/fallback/ProductDegradeFeignClient.java
@@ -2,12 +2,32 @@ package com.atguigu.gmall.fallback;
 
 import com.atguigu.gmall.feignClient.ProductFeignClient;
 import com.atguigu.gmall.model.product.SkuInfo;
+import com.atguigu.gmall.model.product.SpuSaleAttrValue;
 import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Map;
 
 @Component
 public class ProductDegradeFeignClient implements ProductFeignClient {
     @Override
     public SkuInfo getSkuInfo(Long skuId) {
+        return null;
+    }
+
+    @Override
+    public Map<String, Object> getCategoryView(Long category3Id) {
+        return null;
+    }
+
+    @Override
+    public BigDecimal getSkuPrice(Long skuId) {
+        return null;
+    }
+
+    @Override
+    public List<SpuSaleAttrValue> getSpuSaleAttrListCheckBySku(Long skuId, Long spuId) {
         return null;
     }
 }
